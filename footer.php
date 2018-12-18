823d0bbbf3b537af27d529ad759ba345ac7256e8
Onerym <45489914+Onerym@users.noreply.github.com>
2018-12-18 10:19:56 +0100

    NetHugo

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-                <header>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="CSS/style.css">
-                </header>
                 <footer>
-                       <p>Retour en haut</p><br>
-			<p>Copyright © 2018-2019<br>
-			<I>BlockDesign</I></p>
-                        <i><a href="https://www.google.com">Pour nous contacter</a></i></p><br>
+                        <h4 class="lien"><a href="#">Retour en haut</a></h4><br>
+						<h4>Copyright © 2018-2019</h4>
+                        <h4><I>BlockDesign</I></h4><br>
+						<div id="socialbar">
+							<a href="https://www.facebook.com/"><img src="images/logo-facebook-noir-et-blanc-4.png" class="social" alt="image de Facebook"></a>
+							<a href="https://twitter.com/login?lang=fr"><img src="images/logo-twitter-noir.png" class="social" alt="image de Twitter"></a>
+							<a href="https://www.instagram.com/accounts/login/?hl=fr"><img src="images/Instagram_icon-icons.com_66804.png" class="social" alt="image de Twitter"></a>
+						</div>
 		</footer>
 	</body>
 </html>
--- header.php
+++ header.php
@@ -1,12 +1,89 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php
+// Initialize the session
+session_start();
+ 
+
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
 
 
-    <head>
-        <meta charset="UTF-8">
-        <title>Blog Epsi B1</title>
-        <link rel="stylesheet" href="css/styles.css">
-    </head>
 
 	<body>
             <header>
@@ -20,21 +97,33 @@
                         <li><a href="index.php">Accueil</a></li>
                         <li><a href="contact.php">Contact</a></li>
                     </ul>
-            <div id='connexion'>
-                Connexion à l'espace membre :<br />
-                <form action="index.php" method="post">
-                Login : <input type="text" name="login" placeholder="Login"><br />
-                Mot de passe : <input type="password" name="pass" placeholder="Mot de passe"><br />
-                <input type="submit" name="connexion" value="Connexion">
-                </form>
-                    <a href="inscription.php">Vous inscrire</a>
-                <?php
-                    if (isset($erreur)) echo '<br /><br />',$erreur;
-                ?>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
             </div>
-            <div id="membre">
-                <a href="membre.php"><input type="button" name="espace membrebre"value="espace membre"/></a>
-            </nav>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>
+                    <a href="welcome.php"><input type="submit" class="btn btn-primary" value="Espace membre"/></a>
+                    
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
             </header>	
         </body>
 </html>
